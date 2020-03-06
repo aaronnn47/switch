5247998d238f467ed6c90787e68a6a60e85052b7
Aaron Kim <iamaaronkim@gmail.com>
2020-03-06 10:19:54 -0700

    added image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Switch from './Images/switch.png'
 
 function App() {
   return (
     <div className="App">
+      <img src={Switch} alt=''/>
+
+      <div className="text">
       Coming soon
+      </div>
     </div>
+    
   );
 }
 
